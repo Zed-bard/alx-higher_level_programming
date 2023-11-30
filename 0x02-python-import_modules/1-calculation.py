ce78361c5178165ffb0eb305482766b2d220c6b6
Zed-bard <zerihunbard@gmail.com>
2023-11-30 01:15:22 -0800

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """Importing the necessary functions from the calculator_1 module """
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    """ Printing the addition,subtraction, multiplication, Devision"""
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
