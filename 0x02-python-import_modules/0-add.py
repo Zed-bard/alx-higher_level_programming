a227d72bb5b185dca7e4e85855df2975b9b1acd2
Zed-bard <zerihunbard@gmail.com>
2023-11-30 01:06:35 -0800

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,27 +1,6 @@
 #!/usr/bin/python3
-
-"""
-This program demonstrates the addition of two numbers.
-
-Example:
-    python add.py
-"""
-
-def add(a, b):
-    """
-    Adds two numbers.
-
-    Args:
-        a (int): The first number.
-        b (int): The second number.
-
-    Returns:
-        int: The sum of the two numbers.
-    """
-    return a + b
-
 if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-
