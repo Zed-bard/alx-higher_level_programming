f75f0860c2edcbd239fcac7a5d76aa09dc6862c8
Zed-bard <zerihunbard@gmail.com>
2023-11-30 01:43:16 -0800

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-
 def add_arg(argv):
-    # Count the number of arguments passed (excluding the script name itself)
     n = len(argv) - 1
-
-    # If no arguments are provided, print 0 and return
     if n == 0:
         print("{:d}".format(n))
         return
     else:
         i = 1
         add = 0
-        # Iterate over the arguments and add them
         while i <= n:
             add += int(argv[i])
             i += 1
-        # Print the sum of the arguments
         print("{:d}".format(add))
 
 if __name__ == "__main__":
     import sys
-    # Call the add_arg function with the command-line arguments
     add_arg(sys.argv)
