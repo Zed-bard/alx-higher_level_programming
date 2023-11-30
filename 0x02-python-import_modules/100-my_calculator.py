339c1dbbf2f78cba135fae4010b7e0e4d8626cfe
Zed-bard <zerihunbard@gmail.com>
2023-11-30 02:31:50 -0800

    100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
+"""A simple calculator program that imports all functions from calculator_1.py"""
 
-# Importing the necessary module
-import sys
+if __name__ == "__main__":
+    from calculator_1 import *
+    import sys
 
-# Checking the number of command-line arguments
-if len(sys.argv) != 4:
-    print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
-    sys.exit(1)
+    argc = len(sys.argv) - 1
+    if argc != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-# Importing the necessary functions from the calculator_1 module
-from calculator_1 import add, sub, mul, div
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    operator = sys.argv[2]
 
-# Parsing the command-line arguments
-a = int(sys.argv[1])
-operator = sys.argv[2]
-b = int(sys.argv[3])
-result = 0
+    if operator == "+":
+        result = add(a, b)
+    elif operator == "-":
+        result = sub(a, b)
+    elif operator == "*":
+        result = mul(a, b)
+    elif operator == "/":
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-# Performing the desired operation based on the provided operator
-if operator == "+":
-    result = add(a, b)
-elif operator == "-":
-    result = sub(a, b)
-elif operator == "*":
-    result = mul(a, b)
-elif operator == "/":
-    result = div(a, b)
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
+    print("{} {} {} = {}".format(a, operator, b, result))
 
-# Printing the result of the operation
-print("{} {} {} = {}".format(a, operator, b, result))
