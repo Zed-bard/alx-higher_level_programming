48244669c9d4c46d93dc4eeed1ff991015c85c4d
Zed-bard <zerihunbard@gmail.com>
2023-12-04 05:13:51 -0800

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    mapping = {"c": "", "C": ""}
-    return my_string.translate(mapping)
-
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string
