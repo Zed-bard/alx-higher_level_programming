23ec9b6fa10ef586432bf9ca3c3bc612cefc56a6
Zed-bard <zerihunbard@gmail.com>
2023-12-04 06:11:52 -0800

    7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,18 +1,7 @@
+#!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-  """
-  Adds two tuples element-wise, assuming missing elements are 0.
-
-  Args:
-    tuple_a: The first tuple (default empty).
-    tuple_b: The second tuple (default empty).
-
-  Returns:
-    A new tuple with the element-wise sums, handling missing elements.
-  """
-
-  # Pad the tuples with 0s to ensure they're at least 2 elements long.
-  tuple_a = tuple_a[:2] + (0,) * (2 - len(tuple_a))
-  tuple_b = tuple_b[:2] + (0,) * (2 - len(tuple_b))
-
-  # Add the corresponding elements and return the result as a tuple.
-  return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    new_tuple = ()
+    tuple_1 = tuple_a + (0, 0)
+    tuple_2 = tuple_b + (0, 0)
+    new_tuple = tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1]
+    return new_tuple
