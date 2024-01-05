f87ea3bcde36d514fdd4237a9f7d2f3494b75156
Zed-bard <zerihunbard@gmail.com>
2024-01-05 09:50:55 -0800

    100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    result = []  # Store repeated strings efficiently
-    for i in range(len(result)):  # Access current length to determine repetitions
-        result.append("BestSchool")
-    return ", ".join(result)  # Combine strings with commas
+def magic_string(my_list=[]):
+    my_list += ['Coding']
+    return ", ".join(my_list)
