b773403826383a486707eb5cd0ded91f1817a394
Zed-bard <zerihunbard@gmail.com>
2024-01-09 01:54:56 -0800

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
+# 101-stats.py
 """Reads from standard input and computes metrics.
-
 After every ten lines or the input of a keyboard interruption (CTRL + C),
 prints the following statistics:
     - Total file size up to that point.
     - Count of read status codes up to that point.
 """
 
+
 def print_stats(size, status_codes):
     """Print accumulated metrics.
-
     Args:
         size (int): The accumulated read file size.
         status_codes (dict): The accumulated count of status codes.
     """
-    print(f"File size: {size}")
-    for key, value in sorted(status_codes.items()):
-        print(f"{key}: {value}")
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
 
 if __name__ == "__main__":
     import sys
 
     size = 0
     status_codes = {}
-    valid_codes = {'200', '301', '400', '401', '403', '404', '405', '500'}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
     count = 0
 
     try:
@@ -43,7 +43,10 @@ if __name__ == "__main__":
 
             try:
                 if line[-2] in valid_codes:
-                    status_codes[line[-2]] = status_codes.get(line[-2], 0) + 1
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
             except IndexError:
                 pass
 
