de952644a125010a4ec8cf9133703d3fc34c7cfd
Zed-bard <zerihunbard@gmail.com>
2024-01-15 09:09:28 -0800

    base.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,17 +1,5 @@
 #!/usr/bin/python3
 
-"""Defines unittests for base.py
-
-Unittest classes:
-    TestBase_instantiation - line 23
-    TestBase_to_json_string - line 110
-    TestBase_save_to_file - line 156
-    TestBase_from_json_string - line 234
-    TestBase_create - line 288
-    TestBase_load_from_file - line 340
-    TestBase_save_to_file_csv - line 406
-    TestBase_load_from_file_csv - line 484
-"""
 import os
 import unittest
 from models.base import Base
@@ -20,7 +8,7 @@ from models.square import Square
 
 
 class TestBase_instantiation(unittest.TestCase):
-    """Unittests for testing instantiation of the Base class."""
+    """Unittests for testing instantiation of  Base class."""
 
     def test_no_arg(self):
         b1 = Base()
@@ -153,7 +141,7 @@ class TestBase_to_json_string(unittest.TestCase):
 
 
 class TestBase_save_to_file(unittest.TestCase):
-    """Unittests for testing save_to_file method of Base class."""
+    """Unittests for testing save_to_file method Base class."""
 
     @classmethod
     def tearDown(self):
@@ -547,4 +535,3 @@ class TestBase_load_from_file_csv(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
