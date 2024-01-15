884c422e64f5f2cd5339f93a9c4b56189ea13745
Zed-bard <zerihunbard@gmail.com>
2024-01-15 07:24:46 -0800

    square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,151 +1,85 @@
 #!/usr/bin/python3
-"""First Rectangle"""
 
-from models.base import Base
+"""Defines a class square"""
+from models.rectangle import Rectangle
 
 
-class Rectangle(Base):
-    """A rectangle class that inherits from
-    the base class
+class Square(Rectangle):
+    """Represent a square."""
 
-    Args:
-        Base(model): the inherited model
-
-    """
-
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """Constructs the rectange's attributes
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a new Square.
 
         Args:
-            height(int): rectangle's height
-            width(int): rectangle's width
-            x(int): input value
-            y(int): input value
-
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
         """
-        super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        super().__init__(size, size, x, y, id)
 
     @property
-    def width(self):
-        """Gets the Width of the rectangle"""
-        return self.__width
+    def size(self):
+        """Get/set the size of the Square."""
+        return self.width
 
-    @width.setter
-    def width(self, value):
-        """Sets the Width (private) of the rectangle
-
-        Args:
-            value(int): value of the rectangle
-
-        """
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        """Gets the height (private) of the rectangle"""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """Sets the height(private) of the rectangle
-
-          Args:
-            value(int): value of the rectangle
-
-        """
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
-
-    @property
-    def x(self):
-        """Gets the x value"""
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        """Sets x value
-
-         Args:
-            value(int): x value
-
-        """
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = value
-
-    @property
-    def y(self):
-        """Gets the x value"""
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        """Sets y value
-
-         Args:
-            value(int): y value
-
-        """
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = value
-
-    def area(self):
-        """Calculates the area of the rectangle"""
-        return self.__height * self.__width
-
-    def display(self):
-        """Prints the Rectangle instance with the character #"""
-        print("\n" * self.y,  end="")
-        print((" " * self.x + "#" * self.__width + '\n') * self.__height, end="")
-
-    def __str__(self):
-        """String representation of the rectangle class"""
-        str_res = "[Rectangle] ({}) {}/{} - {}/{}"\
-            .format(self.id, self.x, self.y, self.__width, self.__height)
-        return str_res
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
 
     def update(self, *args, **kwargs):
-        """Updates rectangle class and assigns an argument to each attribute
+        """Update the Square.
 
         Args:
-            *args(args):
-            **kwargs(kwargs):5-main.py
-
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents size attribute
+                - 3rd argument represents x attribute
+                - 4th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
         """
-        if not args and not kwargs:
-            return
-        if args is not None:
-            attributes = ["id", "width", "height", "x", "y"]
-            for i, j in enumerate(args):
-                if i < len(attributes):
-                    setattr(self, attributes[i], j)
-        else:
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.size = arg
+                elif a == 2:
+                    self.x = arg
+                elif a == 3:
+                    self.y = arg
+                a += 1
+
+        elif kwargs and len(kwargs) != 0:
             for k, v in kwargs.items():
-                if hasattr(self, k):
-                    setattr(self, k, v)
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
 
     def to_dictionary(self):
-        """Returns the dictionary representation of a rectangle"""
-        _map = {}
-        for key, value in self.__dict__.items():
-            if key.startswith("_"):
-                _map[key.split("__")[-1]] = value
-            else:
-                _map[key] = value
-        return _map
+        """Return the dictionary representation of the Square."""
+        return {
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
+        }
+
+    def __str__(self):
+        """Return the print() and str() representation of a Square."""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
+
