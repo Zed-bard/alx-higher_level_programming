0643d1081000bad9d1b345c4f3b88d80154318fe
Zed-bard <zerihunbard@gmail.com>
2024-03-17 08:01:31 -0700

    6-model_state

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -1,35 +1,11 @@
 #!/usr/bin/python3
+"""Start link class to table in database 
 """
-Module: 6-model_state
-
-Contains the definition of the State class and creates the corresponding table in the database.
-"""
-
 import sys
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
+from model_state import Base, State
 
-Base = declarative_base()
-
-class State(Base):
-    """
-    State class inherits from Base.
-
-    Attributes:
-        __tablename__: Name of the table in the database.
-        id: Integer column representing the primary key.
-        name: String column representing the state name.
-    """
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    name = Column(String(128), nullable=False)
+from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    """
-    Main entry point of the script.
-    Connects to the MySQL server and creates the necessary table.
-    """
-    # Connect to MySQL server running on localhost at port 3306
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
